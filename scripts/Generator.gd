dcbd19ce8e8a4d2949a1fa52be4a73ef886d8ee4
Joaquim <joca.lucena@gmail.com>
2022-01-15 15:48:53 -0300

    Added some options to the world generator, including size, mountain height, and others. LGTM.

--- scripts/Generator.gd
+++ scripts/Generator.gd
@@ -3,13 +3,18 @@ extends Node2D
 
 export var size_x : int = 256
 export var size_y : int = 256
-
+export var noise_seed : int = 1
+export var octaves  : int = 4
+export var period : float = 100.0
+export var persistence : float = 0.5
 
 var noise = OpenSimplexNoise.new()
+
+
 var tile :int = 0
 pass
 
-export var test = 1.0
+export var height_modifier = 1.0
 export var raiser = 2.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -42,20 +47,62 @@ func get_tile_color(perlin):
 	#i
 	#pass
 func generate():
-	randomize()
-	noise.seed = randi()
-	noise.octaves = 4
-	noise.period = 100.0
-	noise.persistence = 0.5
+	$TileMap.clear()
+	noise.seed = noise_seed
+	noise.octaves = octaves
+	noise.period = period
+	noise.persistence = persistence
+	
 	
 	
 	
 	for x in range(0,size_x):
 		for y in range(0,size_y):
-				tile = get_tile_color(noise.get_noise_2d(x,y) * test)
+				tile = get_tile_color(noise.get_noise_2d(x,y) * height_modifier)
 				$TileMap.set_cell(x,y,tile)
 
 
 func _input(event):
 	if Input.is_key_pressed(KEY_R):
 		generate()
+
+
+func _on_small_pressed():
+	size_x = 128
+	size_y = 128
+
+
+func _on_medium_pressed():
+	size_x = 256
+	size_y = 256
+
+
+func _on_large_pressed():
+	size_x = 512
+	size_y = 512
+
+func _on_persistence_value_value_changed(value):
+	persistence = value
+	$GUI/MarginContainer/PanelContainer/VBoxContainer/persistence/persistence.set_text("Persistence : " +  String(value))
+
+func _on_octave_slider_value_changed(value):
+	octaves = value
+	$GUI/MarginContainer/PanelContainer/VBoxContainer/octaves/octave.set_text("Octaves : " +  String(value))
+
+func _on_periodSpin_value_changed(value):
+	period = value
+	
+func _on_seed_box_value_changed(value):
+	noise_seed = value
+	
+func _on_generate_pressed():
+	generate()
+	
+func _on_flatter_pressed():
+	height_modifier = 1.0
+	
+func _on_normal_pressed():
+	height_modifier = 1.5
+
+func _on_peaks_pressed():
+	height_modifier = 2.0
